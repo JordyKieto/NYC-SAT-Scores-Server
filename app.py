4c2f2e5befc99ab277d58e509e6016facd2e866b
Jordy Kieto <kieto@ualberta.ca>
2019-07-22 11:01:41 -0400

    manually merged sqlite branch

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
+
 from flask import Flask, request, send_file
-import pandas as pd
-import math
 from utils.filter import Filter
 from utils.formatResponse import formatResponse
 
-filter = Filter(pd.read_csv('scores.csv'))
+filter = Filter('pythonsqlite.db')
 
 app = Flask(__name__)
 
